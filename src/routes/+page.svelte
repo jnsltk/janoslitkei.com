1980313c17eb279bf6ae0b8d0b35a8733e66ad67
jnsltk <janos.litkei@gmail.com>
2023-06-07 18:58:38 +0200

    #2 Add text, layout, styling and fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1 class="text-3xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col justify-between h-screen">
+    <div class="mt-20 ml-16">
+        <div class="mb-8">
+            <h1 class="text-6xl font-display mb-1">Janos Litkei</h1>
+            <h2 class="text-xl">Software Engineering student based in Gothenurg</h2>
+        </div>
+        <div>
+            <ul class="text-2xl underline font-bold">
+                <li>Projects</li>
+                <li>About Me</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+    <div class="dot mb-16 ml-16" />
+</div>
+
+<style>
+    .dot {
+        height: 35px;
+        width: 35px;
+        background-color: #D4815D;
+        border-radius: 50%;
+        display: inline-block;
+    }
+</style>
