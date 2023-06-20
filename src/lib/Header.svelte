a4c9eefabaf623f45dfb0c610676247f1d7c066d
jnsltk <janos.litkei@gmail.com>
2023-06-20 09:53:17 +0200

    #9 Create first version of animated blobs

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,10 +1,10 @@
-<div class="fixed top-0 left-0 right-0 pt-12 mx-16 flex justify-between z-20">
+<div class="fixed top-0 left-0 right-0 pt-12 mx-16 flex justify-between z-20 bg-gradient-to-b from-light-bg-300 from-85% to-[rgba(0,0,0,0)]">
     <div class="">
         <h1 class="text-4xl font-display mb-4">Janos Litkei</h1>
         
     </div>
     <div class="">
-        <ul class=" mt-2 text-base font-semibold flex gap-10">
+        <ul class=" mt-2 text-base font-semibold flex gap-10 ">
             <li><a class="hover:border-b-4 border-black" href="#home">Home</a></li>
             <li><a class="hover:border-b-4 border-black" href="#about">About Me</a></li>
             <li><a class="hover:border-b-4 border-black" href="#projects">Projects</a></li>
@@ -12,9 +12,3 @@
         </ul>
     </div>
 </div>
-
-<style>
-    div {
-        background-color: #EEE7D8;
-    }
-</style>
